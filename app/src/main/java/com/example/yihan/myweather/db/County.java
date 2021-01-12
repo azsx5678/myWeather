37b583ffa76a4bbf12dda218b0dfaafed3225b38
yihanz1 <719351071@qq.com>
2021-01-12 20:47:36 +0800

    省县市

---
+++ app/src/main/java/com/example/yihan/myweather/db/County.java
@@ -0,0 +1,42 @@
+package com.example.yihan.myweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class County extends DataSupport {
+    private  int id;
+    private String countName;
+    private  String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCouuntName() {
+        return countName;
+    }
+
+    public void setCountName(String couuntName) {
+        this.countName = couuntName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
